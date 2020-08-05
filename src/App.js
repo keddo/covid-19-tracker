1852922487e8ece5afe1203062fea54fbaebbacf
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-05 14:03:16 +0300

    Fetch country data from api

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
 
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -27,9 +28,15 @@ function App() {
     getCountriesData();
   }, [])
 
-  const onCountryChange = (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    setCountry(countryCode);
+    const url = countryCode == 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+    await fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            setCountry(countryCode);
+            setCountryInfo(data);
+          });
   }
   return (
     <div className="app">
