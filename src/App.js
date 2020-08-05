5a1539ef04c6905db73d2000f39559243dbac982
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-05 16:09:50 +0300

    Add line graph component to the app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
+import LineGraph from './LineGraph';
+
+// Utitlity 
+import {sortData} from './util';
 
 function App() {
   //https://disease.sh/v3/covid-19/countries
@@ -30,7 +34,8 @@ function App() {
                   name: country.country,
                   value: country.countryInfo.iso2
                 }));
-                setTableData(data);
+                const sortedData = sortData(data);
+                setTableData(sortedData);
                 setCountries(countries);
             });
 
@@ -86,6 +91,7 @@ function App() {
           </CardContent>
         </Card>
       {/* Graph */}
+      <LineGraph/>
       </div>
       
     </div>
