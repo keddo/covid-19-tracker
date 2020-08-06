76b50eacbab55a8aafd205f09f4bfc62d61a5721
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-06 12:54:46 +0300

    Add react-leaflet and leaflet packages for map

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Map as LeafletMap, TileLayer} from 'react-leaflet'
 
 function Map() {
     return (
         <div className="map">
-            <h1>I am a map!</h1>
+            <LeafletMap>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+            </LeafletMap>
         </div>
     )
 }
